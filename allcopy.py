0d0431ee7c0cda94727f94a517974d5482d263e4
Zsolti37 <petozsolt33@gmail.com>
2016-09-23 20:43:32 +0200

    Copy all file from to

---
+++ allcopy.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3.5
+# -*- coding: UTF-8 -*-
+
+import os
+import shutil
+os.chdir('C:\\') #Make sure you add your source and destination path below
+
+dir_src = (r"D:\Dokumentumok\Nekem\onedrive\OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség\Python\\")
+dir_dst = (r"D:\raspberrypi\Python\\")
+
+print("Másolás innen:")
+print(dir_src)
+print("Ide:")
+print(dir_dst)
+print()
+
+lista = os.listdir(dir_src)
+kiterjesztes=".py"
+
+for filename in lista:
+    if filename.endswith(kiterjesztes):
+        shutil.copy( dir_src + filename, dir_dst)
+        print("A következő fájl másolása:", filename)
